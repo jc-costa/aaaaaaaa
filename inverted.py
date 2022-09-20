f7873e6819cec2e21c362226f83f0ac7b2ce2c3c
Jefferson Costa <jefficostau@gmail.com>
2022-09-20 19:09:20 -0300

    Update inverted.py

--- inverted.py
+++ inverted.py
@@ -1,6 +1,8 @@
+import pandas as pd
+from pathlib import Path
+import pickle
 import json
 import os
-from pathlib import Path
 import csv 
 
 five_keys = ["marca", "modelo", "sistema operacional", "processador", "cor"]
@@ -13,29 +15,37 @@ path = os.path.join(download_folder, 'extractor/results')
 filelist = [os.path.join(path, file) for file in os.listdir(path)]
 filenames = os.listdir(path)
 
-#using five keys to create a csv file
-def create_csv(json_file, keys):
-    with open(json_file, 'r') as f:
-        data = json.load(f)
-    #create a csv file
-    with open('csv_file1.csv', 'w') as csv_file:
-        writer = csv.writer(csv_file)
-        #write the keys
-        writer.writerow(keys)
-        #write the values in the csv file
-        writer.writerow([data[key] for key in keys])
+appended_data = []
 
 for filename, filepath in zip(filenames, filelist):
     try:
         with open(filepath, encoding='utf-8') as info_json:
             data = json.load(info_json)
-            create_csv(data, five_keys)
+            #select data using five keys and create a csv file
+            data = {key: data[key] for key in five_keys}
+            #create a pandas dataframe from dicionaries in a for loop
+            df = pd.DataFrame(data, index=[0])
+            #append the dataframes
+            appended_data.append(df)
+            #print(type(data))            
     except:
         pass
 
-#creating a csv with five keys in which the keys are columns and the values are rows
-#using five keys to create a csv file from json file
-#
+#create a csv from a list of pandas dataframes
+def create_csv_from_list(list_of_dataframes):
+    with open('csv_file2.csv', 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        #write the keys
+        writer.writerow(list_of_dataframes[0].keys())
+        #write the values in the csv file
+        for df in list_of_dataframes:
+            writer.writerow(df.values[0])
+
+#concatenate the dataframes
+appended_data = pd.concat(appended_data, ignore_index=True)
+print(appended_data)
+#create csv from pandas dataframe
+appended_data.to_csv('csv_file4.csv', index=False)
 
 #rename the keys in json file
 def rename_key(json_file, key, new_key):  
@@ -47,8 +57,42 @@ def rename_key(json_file, key, new_key):
     with open(json_file, 'w') as f:
         #dump the json file
         json.dump(data, f, indent=4)
-        
-five_keys = ["marca", "modelo", "sistema operacional", "processador", "cor"]
 
+#inverted index with the frequency of the term from a csv file
+def inverted_index_with_frequency(csv_file):
+    #read the csv file
+    df = pd.read_csv(csv_file)
+    #create a dictionary
+    inverted_index = {}
+    #iterate over the rows in the dataframe
+    for index, row in df.iterrows():
+        #iterate over the columns in the dataframe
+        for column in df:
+            #get the value in the cell
+            value = row[column]
+            #if the value is not null
+            if pd.notnull(value):
+                #if the value is not in the dictionary
+                if value not in inverted_index:
+                    #add the value in the dictionary
+                    inverted_index[value] = {}
+                #if the index of the row is not in the dictionary
+                if index not in inverted_index[value]:
+                    #add the index of the row in the dictionary
+                    inverted_index[value][index] = 1
+                else:
+                    #add the frequency of the term
+                    inverted_index[value][index] += 1
+    #return the dictionary
+    return inverted_index
+
+inverted_index = inverted_index_with_frequency('csv_file4.csv')
+
+#save the inverted index in a json file
+#save_inverted_index(inverted_index, 'inverted_index.json')
+
+#save inverted index in a binary file
+with open('inverted_index.bin', 'wb') as f:
+    pickle.dump(inverted_index, f)
 
 
