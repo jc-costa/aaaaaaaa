92aefffcbc29d459bc5b6991abf7670861b8acb1
Jefferson Costa <jefficostau@gmail.com>
2022-09-24 18:46:51 -0300

    Update inverted.py

--- inverted.py
+++ inverted.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from pathlib import Path
-import pickle
 import json
 import os
 import csv 
@@ -23,6 +22,7 @@ for filename, filepath in zip(filenames, filelist):
             data = json.load(info_json)
             #select data using five keys and create a csv file
             data = {key: data[key] for key in five_keys}
+            #print(data)
             #create a pandas dataframe from dicionaries in a for loop
             df = pd.DataFrame(data, index=[0])
             #append the dataframes
@@ -30,35 +30,28 @@ for filename, filepath in zip(filenames, filelist):
             #print(type(data))            
     except:
         pass
+    
+#tokenize list of value by hifen
+def tokenize_hifen(value):
+    #create a list
+    list_of_values = []
+    #iterate over the list
+    for v in value:
+        #if the value has hifen
+        if '-' in v:
+            #split the value
+            v = v.split('-')
+            #iterate over the list
+            for i in v:
+                #add the value in the list
+                list_of_values.append(i)
+        else:
+            #add the value in the list
+            list_of_values.append(v)
+    #return the list
+    return list_of_values
 
-#create a csv from a list of pandas dataframes
-def create_csv_from_list(list_of_dataframes):
-    with open('csv_file2.csv', 'w') as csv_file:
-        writer = csv.writer(csv_file)
-        #write the keys
-        writer.writerow(list_of_dataframes[0].keys())
-        #write the values in the csv file
-        for df in list_of_dataframes:
-            writer.writerow(df.values[0])
-
-#concatenate the dataframes
-appended_data = pd.concat(appended_data, ignore_index=True)
-print(appended_data)
-#create csv from pandas dataframe
-appended_data.to_csv('csv_file4.csv', index=False)
-
-#rename the keys in json file
-def rename_key(json_file, key, new_key):  
-    with open(json_file, 'r') as f:
-        #load the json file
-        data = json.load(f)
-    #rename the key
-    data[new_key] = data.pop(key)
-    with open(json_file, 'w') as f:
-        #dump the json file
-        json.dump(data, f, indent=4)
-
-#inverted index with the frequency of the term from a csv file
+#tokanize values in a csv file
 def inverted_index_with_frequency(csv_file):
     #read the csv file
     df = pd.read_csv(csv_file)
@@ -72,27 +65,140 @@ def inverted_index_with_frequency(csv_file):
             value = row[column]
             #if the value is not null
             if pd.notnull(value):
-                #if the value is not in the dictionary
-                if value not in inverted_index:
-                    #add the value in the dictionary
-                    inverted_index[value] = {}
-                #if the index of the row is not in the dictionary
-                if index not in inverted_index[value]:
-                    #add the index of the row in the dictionary
-                    inverted_index[value][index] = 1
-                else:
-                    #add the frequency of the term
-                    inverted_index[value][index] += 1
+                #split the value in a list
+                value = value.split()
+                #tokenize the value by hifen
+                value = tokenize_hifen(value)
+                #iterate over the list
+                for term in value:
+                    term = term+"_"+column
+                    #if the term is not in the dictionary
+                    if term not in inverted_index:
+                        #add the term in the dictionary
+                        inverted_index[term] = {}
+                    #if the index of the row is not in the dictionary
+                    if index not in inverted_index[term]:
+                        #add the index of the row in the dictionary
+                        inverted_index[term][index] = 1
+                    else:
+                        #add the frequency of the term
+                        inverted_index[term][index] += 1
     #return the dictionary
     return inverted_index
 
+#create a csv from a list of pandas dataframes
+def create_csv_from_list(list_of_dataframes):
+    with open('csv_file2.csv', 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        #write the keys
+        writer.writerow(list_of_dataframes[0].keys())
+        #write the values in the csv file
+        for df in list_of_dataframes:
+            writer.writerow(df.values[0])
+
+#concatenate the dataframes
+appended_data = pd.concat(appended_data, ignore_index=True)
+#print(appended_data)
+#create csv from pandas dataframe
+appended_data.to_csv('csv_file4.csv', index=False)
+
+#renaming the keys in json file
+def rename_key(json_file, key, new_key):  
+    with open(json_file, 'r') as f:
+        #load the json file
+        data = json.load(f)
+    #rename the key
+    data[new_key] = data.pop(key)
+    with open(json_file, 'w') as f:
+        #dump the json file
+        json.dump(data, f, indent=4)
+
+#save inverted index in a json
+def save_inverted_index_json(inverted_index):
+    #open the json file
+    with open('inverted_index.json', 'w') as f:
+        #dump the inverted index in the json file
+        json.dump(inverted_index, f, indent=4)
+
 inverted_index = inverted_index_with_frequency('csv_file4.csv')
 
-#save the inverted index in a json file
-#save_inverted_index(inverted_index, 'inverted_index.json')
+save_inverted_index_json(inverted_index)
+
+#save a dictionary in a binary file 
+def save_inverted_index_binary(inverted_index):
+    #open the binary file
+    with open('inverted_index_without_compreesion.bin', 'wb') as f:
+        #write the inverted index in the binary file
+        f.write(str(inverted_index).encode('utf-8'))
+    f.close()
 
 #save inverted index in a binary file
-with open('inverted_index.bin', 'wb') as f:
-    pickle.dump(inverted_index, f)
 
+index_without = save_inverted_index_binary(inverted_index)
 
+#load a dictionary from a binary file 
+def load_inverted_index_binary():
+    #open the binary file
+    with open('inverted_index_without_compreesion.bin', 'rb') as f:
+        #read the inverted index from the binary file
+        inverted_index = f.read().decode('utf-8')
+    f.close()
+    #return the inverted index
+    return inverted_index
+
+#compress dictionary into binary file using variable byte encoding
+def compress_dictionary_vbe(dictionary):
+    with open('inverted_index_dic_vbe.bin', 'wb') as f:
+        #write the length of the dictionary
+        f.write(len(dictionary).to_bytes(4, byteorder='big'))
+        #iterate over the dictionary
+        for key, value in dictionary.items():
+            #write the length of the key
+            f.write(len(key).to_bytes(4, byteorder='big'))
+            #write the key
+            f.write(key.encode('utf-8'))
+            #write the length of the value
+            f.write(len(value).to_bytes(4, byteorder='big'))
+            #iterate over the value
+            for key2, value2 in value.items():
+                #write the length of the key
+                f.write(len(str(key2)).to_bytes(4, byteorder='big'))
+                #write the key
+                f.write(str(key2).encode('utf-8'))
+                #write the length of the value
+                f.write(len(str(value2)).to_bytes(4, byteorder='big'))
+                #write the value
+                f.write(str(value2).encode('utf-8'))
+    f.close()
+
+compress_dictionary_vbe(inverted_index)
+
+#retrive a term from a binary file 
+def retrive_term_from_binary_file(term):
+    #open the binary file
+    with open('inverted_index_dic_vbe.bin', 'rb') as f:
+        #read the length of the dictionary
+        length = int.from_bytes(f.read(4), byteorder='big')
+        #iterate over the length of the dictionary
+        for i in range(length):
+            #read the length of the key
+            length_key = int.from_bytes(f.read(4), byteorder='big')
+            #read the key
+            key = f.read(length_key).decode('utf-8')
+            #read the length of the value
+            length_value = int.from_bytes(f.read(4), byteorder='big')
+            #iterate over the length of the value
+            for j in range(length_value):
+                #read the length of the key
+                length_key2 = int.from_bytes(f.read(4), byteorder='big')
+                #read the key
+                key2 = f.read(length_key2).decode('utf-8')
+                #read the length of the value
+                length_value2 = int.from_bytes(f.read(4), byteorder='big')
+                #read the value
+                value2 = f.read(length_value2).decode('utf-8')
+                #if the key is equal to the term
+                if key == term:
+                    #return the value
+                    return value2
+    f.close()
